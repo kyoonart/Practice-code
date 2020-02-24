f84794a48ca7c3ab7fe7321f23f322df8df2969a
pengtaoa <1564328778@qq.com>
2020-02-24 22:17:34 +0800

    update

--- review/23函数节流.js
+++ review/23函数节流.js
@@ -1,3 +1,4 @@
+// 节流
 var processor = {
     timeOutId: null,
     // 实际运行函数
@@ -18,3 +19,38 @@ var processor = {
     }
 }
 processor.process()
+// 防抖动
+_.debounce = function(func, wait, immediate) {
+    var timeout, args, context, timestamp, result;
+    var later = function() {
+      // 现在和上一次时间戳比较
+      var last = _.now() - timestamp;
+      // 如果当前间隔时间少于设定时间且大于0就重新设置定时器
+      if (last < wait && last >= 0) {
+        timeout = setTimeout(later, wait - last);
+      } else {
+        // 否则的话就是时间到了执行回调函数
+        timeout = null;
+        if (!immediate) {
+          result = func.apply(context, args);
+          if (!timeout) context = args = null;
+        }
+      }
+    };
+    return function() {
+      context = this;
+      args = arguments;
+      // 获得时间戳
+      timestamp = _.now();
+      // 如果定时器不存在且立即执行函数
+      var callNow = immediate && !timeout;
+      // 如果定时器不存在就创建一个
+      if (!timeout) timeout = setTimeout(later, wait);
+      if (callNow) {
+        // 如果需要立即执行函数的话 通过 apply 执行
+        result = func.apply(context, args);
+        context = args = null;
+      }
+      return result;
+    };
+  };
