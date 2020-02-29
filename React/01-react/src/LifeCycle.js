3732688c55758d189d62a604c1beff066356cae5
pengtaoa <1564328778@qq.com>
2020-02-29 23:52:25 +0800

    React

--- React/01-react/src/LifeCycle.js
+++ React/01-react/src/LifeCycle.js
@@ -1,9 +1,11 @@
 import React , { Component } from 'react'
 export default class LifeCycle  extends Component{
    render(){
+
        return (
            <div>
                <h2>hello react</h2>
+       <h1>{this.props.arr}</h1>
            </div>
        )
    }
--- React/01-react/src/index.js
+++ React/01-react/src/index.js
@@ -3,12 +3,48 @@ import ReactDom from 'react-dom'
 import LifeCycle from './LifeCycle'
 // const ele = < h2 > hello,react 233</h2>
 const arr=[2,2,5,6,5,5,6]
-
-ReactDom.render(<LifeCycle></LifeCycle>,document.querySelector('#root'));
+console.log(arr);
+ReactDom.render(<LifeCycle arr={arr}></LifeCycle>,document.querySelector('#root'));
+class Clock extends React.Component {
+    constructor(props){
+    super(props);
+    this.state={date:new Date()};
+    }
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        </div>
+      );
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      };
+    
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      };
+    
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+      };
+  }
+ 
+ 
+    ReactDom.render(
+      <Clock/>,
+      document.getElementById('roo')
+    );
 // ReactDom.render(<ul>
 //     {
 //     arr.map((item,index)=>{
 //         return <li key={index}>{item}</li>
 //     })
 //     }
-// </ul>, document.querySelector('#root'));
+// </ul>, document.querySelector('#root')); 
