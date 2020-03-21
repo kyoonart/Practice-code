36b7d406d314fd56c7c3b667f3dd774bc2460e0a
pengtaoa <1564328778@qq.com>
2020-03-21 23:21:56 +0800

    update

--- review/剑指offer/反转字符串.js
+++ review/剑指offer/反转字符串.js
@@ -3,12 +3,3 @@ function ReverseSentence(str) {
     if (!str) return '';
     return str.split(' ').reverse().join(' ');
 }
-
-function IsContinuous(numbers) {
-    // write code here
-    let A = 0;
-    let B = 0;
-    for (var i = 0; i < numbers.length; i++) {
-
-    }
-}
