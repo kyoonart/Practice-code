d2cd2ffc774793ad2e5e4dd72063778d028357ee
pengtaoa <1564328778@qq.com>
2020-03-24 23:30:38 +0800

    update

--- review/01.js
+++ review/01.js
@@ -1,10 +1,11 @@
-// // console.log(typeof null);
-// // var num = parseInt("10", 2)
-// // console.log(num);
-// var a = 2;
-// // console.log(++a + 1);
-// console.log(--a);
-// console.log(a);
+console.log(typeof null);
+var num = parseInt("10", 2)
+console.log(num);
+var a = 2;
+console.log(++a + 1);
+console.log(--a);
+console.log(a);
+
 function setName(obj) {
     obj.name = 'zhangsan';
     obj = new Object()
