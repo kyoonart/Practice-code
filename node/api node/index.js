2de9028048fb65e53e26348bcd314171234a7402
pengtaoa <1564328778@qq.com>
2020-05-29 23:51:03 +0800

    update

--- node/api node/index.js
+++ node/api node/index.js
@@ -3,9 +3,12 @@ const app = express();
 // express实例化
 // 最简单的api接口
 app.get('/user/login', (req, res) => {
-    console.log('hello');
-    res.send('注册ok');
-    console.log(23);
+    // console.log('hello');
+    // res.send('注册ok');
+    // console.log(23);
+    res.send({ code: 200, msg: 'everything is ok', data: 'okoko' });
+    console.log(req.query);
+
 
 })
 app.listen(3000, () => {
