5a0cdd7e62e5984d9279536bc92d6dac722f48cb
pengtaoa <1564328778@qq.com>
2020-06-17 19:10:04 +0800

    update

--- TypeScript/t/函数.js
+++ TypeScript/t/函数.js
@@ -1,24 +1,23 @@
 var mySum = function (x, y) {
-    return x + y;
+  return x + y;
 };
 mySum(1, 3);
 var mySearch;
 mySearch = function (source, subString) {
-    return source.search(subString) !== -1;
+  return source.search(subString) !== -1;
 };
 var Sumx = function (x, y) {
-    return x + y;
+  return x + y;
 };
 //剩余参数
 function push(array) {
-    var items = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        items[_i - 1] = arguments[_i];
-    }
-    items.forEach(function (item) {
-        array.push(item);
-    });
-    return array;
+  var items = [];
+  for (var _i = 1; _i < arguments.length; _i++) {
+    items[_i - 1] = arguments[_i];
+  }
+  items.forEach(function (item) {
+    array.push(item);
+  });
+  return array;
 }
-var a = [];
 push(a, 1, 2, 3);
